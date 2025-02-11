80cd36b9176ca4fafb70cf28383d386af9c65f45
erika <erika@ERIKA>
2025-02-11 13:51:18 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TechGirls1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
